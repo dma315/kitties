5da6e947bde85e6e3a796687e6cfbb483d5f8f92
dma315 <dma315@gmail.com>
2016-03-10 17:16:05 -0500

    Add vote creation functionality

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,11 +1,22 @@
 class VotesController < ApplicationController
 
   def new
-    @cat1 = Cat.all.sample
-    @cat2 = Cat.all.sample
+    all_cats = Cat.all
+    @cat1 = all_cats.sample
+    @cat2 = all_cats.sample
     while @cat1 == @cat2
-      @cat2 = Cat.all.sample
+      @cat2 = all_cats.sample
     end
   end
-  
+
+  def create
+    Vote.create(
+      winner_id: params["winner_id"].to_i,
+      loser_id: params["loser_id"].to_i
+    )
+    respond_to do |format|
+      format.js { render nothing: true }
+    end
+  end
+
 end
