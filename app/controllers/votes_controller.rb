26fccd9176306d62e172a24421e8bfcfef9e37ec
dma315 <dma315@gmail.com>
2016-03-10 17:52:18 -0500

    Add elo score

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -10,10 +10,20 @@ class VotesController < ApplicationController
   end
 
   def create
+    winner_id = params["winner_id"].to_i
+    loser_id = params["loser_id"].to_i
     Vote.create(
-      winner_id: params["winner_id"].to_i,
-      loser_id: params["loser_id"].to_i
+      winner_id: winner_id,
+      loser_id: loser_id
     )
+    winner = Cat.find_by(id: winner_id)
+    loser = Cat.find_by(id: loser_id)
+
+    p "winner score"
+    winner.update_elo_score(loser, true)
+    p "loser score"
+    loser.update_elo_score(winner, false)
+
     respond_to do |format|
       format.js { render nothing: true }
     end
--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,4 +1,19 @@
 class Cat < ActiveRecord::Base
+
   has_many :winning_votes, foreign_key: :winner_id, class_name: 'Vote'
   has_many :losing_votes, foreign_key: :loser_id, class_name: 'Vote'
+
+  def update_elo_score(opponent, win)
+    k_factor = 32
+    p actual_score = win ? 1 : 0
+
+    my_score = self.elo_score
+    opponent_score = opponent.elo_score
+    score_differential = opponent_score - my_score
+    expected_score = 1.0 / (1 + 10 **( (1.0 * score_differential) / 400))
+
+    updated_score = my_score + k_factor * (actual_score - expected_score)
+    p updated_score
+    
+  end
 end
