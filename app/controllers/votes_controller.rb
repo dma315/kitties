679a4339164448ee740bb3f81e50dcd99c0e7305
dma315 <dma315@gmail.com>
2016-03-10 16:28:22 -0500

    Add bootstrap

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,5 +1,11 @@
 class VotesController < ApplicationController
 
   def new
+    @cat1 = Cat.all.sample
+    @cat2 = Cat.all.sample
+    while @cat1 == @cat2
+      @cat2 = Cat.all.sample
+    end
   end
+  
 end
