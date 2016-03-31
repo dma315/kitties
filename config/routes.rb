bdfefeb7dc8f0037639e73280ec0ce59b9015a1b
dma315 <dma315@gmail.com>
2016-03-31 17:29:52 -0500

    add about page

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
 
   # Static Pages
   get 'privacy' => 'static_pages#privacy'
+  get 'about' => 'static_pages#about'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
