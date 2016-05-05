7563aa6c23807bbd9736964e1e472ec7c3f0b9a1
dma315 <dma315@gmail.com>
2016-05-04 20:48:25 -0700

    Add ability to draw from cached images

--- app/assets/javascripts/next_matchup.js
+++ app/assets/javascripts/next_matchup.js
@@ -1,31 +1,16 @@
-// Pre Vote -> Generate array of 10, cache to 4
-// After Vote 1 -> Cache becomes 3, generate array of 10 more, cache to 8
-// Vote 2 -> Cache becomes 7, no generation of array, 
+// Get 5 cats, put them in the cache
+// Move one up from cache
+// Check if cache has 2 or fewer matchups
 
-function loadFirstMatchup() {
-  generateCatQueue().done(function() {
-    populateVoteBoxes();
-    var $firstMatchup = $('.vote-boxes').first();
-    $firstMatchup.fadeIn();
+function renderInitialMatchup() {
+  getCatQueueData().done(function() {
+    renderCatQueue();
+    renderNextMatchup();
   });
 }
 
-function nextMatchup() {
-  
-  if (CATARRAY <= 4) {
-    loadFirstMatchup();
-  } else {
-    populateVoteBoxes()
-  }
-
-  var $voteBoxes = $('.vote-boxes')
-  var $currentMatchup = $($voteBoxes[0])
-  var $nextMatchup = $($voteBoxes[1])
-  $currentMatchup.remove().fadeOut();
-  $nextMatchup.fadeIn();
-}
-
-function generateCatQueue() {
+// AJAX request to fill the CATARRAY
+function getCatQueueData() {
   var request = $.get({
     url: "/cats/random_cats.json"
   })
@@ -41,7 +26,8 @@ function generateCatQueue() {
   return promise
 }
 
-function populateVoteBoxes() {
+// Creates individual matchup divs, while removing cats from the array
+function createMatchupTemplate() {
   var cat1 = CATARRAY.shift()
   var cat2 = CATARRAY.shift()
   var cats = {
@@ -49,7 +35,23 @@ function populateVoteBoxes() {
     cat2: cat2
   }
   var html = JST['views/voting_boxes'](cats)
-  var $html = $(html).hide();
-  $('#vote-container').append($html)
+  var $html = $(html);
+  return $html
 }
 
+// Renders matchups into a hidden div
+function renderCatQueue() {
+  while (CATARRAY.length > 0) {
+    var $html = createMatchupTemplate()
+    $('#kitty-cache').append($html)
+  }
+}
+
+function renderNextMatchup() {
+  $('#matchup-container').children().remove();
+  var nextMatchup = $('#kitty-cache').children()[0]
+  var $nextMatchup = $(nextMatchup).hide();
+  // console.log($nextMatchup)
+  $('#matchup-container').append($nextMatchup)
+  $nextMatchup.fadeIn();
+}
