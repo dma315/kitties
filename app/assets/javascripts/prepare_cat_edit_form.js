4c6f21d20a8a9212ca09af0dba8edcc1f4dc90c8
dma315 <dma315@gmail.com>
2016-05-09 18:41:30 -0700

    Add clipboard and copy functionality

--- app/assets/javascripts/prepare_cat_edit_form.js
+++ app/assets/javascripts/prepare_cat_edit_form.js
@@ -2,8 +2,11 @@ function prepareCatEditForm() {
   var catID = $(this).closest('.cat-submission-container').data('id')
   var catURL = "/cats/" + catID
   var catName = $(this).closest('.cat-submission-container').data('name')
+  var catMatchupID = $(this).closest('.cat-submission-container').data('matchup-id')
+  var catMatchupURL = "www.elokitty.com/cats/matchup/" + catMatchupID
   $("#edit-cat-form").attr('action', catURL)
   $("#delete-cat-button").attr('action', catURL)
+  $("#share-url").attr('value', catMatchupURL)
   $("#cat-name").attr('value', catName)
 }
 
--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,6 +1,6 @@
 class Cat < ActiveRecord::Base
 
-  attr_accessor :has_cat
+  attr_accessor :has_cat # Added as virtual attribute when checking if there is a cat
 
   has_many :winning_votes, foreign_key: :winner_id, class_name: 'Vote'
   has_many :losing_votes, foreign_key: :loser_id, class_name: 'Vote'
@@ -27,6 +27,9 @@ class Cat < ActiveRecord::Base
     self.save!
   end
 
+  def share_url
+  end
+
   private
 
   def image_has_cat
@@ -45,6 +48,5 @@ class Cat < ActiveRecord::Base
   def to_param
     self.matchup_id
   end
-
 
 end
